a13f4456945cedbcbb7eddf82deb3af1e17d5e24
masashi <>
2022-12-27 23:52:08 +0900

    useStateでカウンター作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
 import Head from "next/head";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Main } from "../components/Main";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-
+  const [count, setCount] = useState(1)
+  const handlClick = (e) => {
+    setCount(count => count + 1);
+  }
   useEffect(() => {
-    console.log('TTTTTTTTTTTTTTTTTTT');
+
     document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
@@ -20,6 +23,8 @@ export default function Home() {
         <title>index</title>
       </Head>
       <Header></Header>
+      <h1>{count}</h1>
+      <button onClick={handlClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
