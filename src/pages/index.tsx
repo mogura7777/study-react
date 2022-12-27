9fd13743815f54c4343400013cb788c676838204
masashi <>
2022-12-27 23:30:11 +0900

    useEffectを利用

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Head from "next/head";
+import { useEffect } from "react";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Main } from "../components/Main";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+
+  useEffect(() => {
+    console.log('TTTTTTTTTTTTTTTTTTT');
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      document.body.style.backgroundColor = "";
+    }
+  }, [])
   return (
     <div className={styles.container}>
       <Head>
